97d68c7be069b918e1381ebc64e578748ce26318
Michelle Scott <scottmm374@gmail.com>
2019-09-07 17:28:55 -0400

    Added link to home button, forgot to do that

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,25 +1,28 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { statement } from '@babel/template';
 
 const Movie = (props) => {
-  
+
   const [movie, setMovie] = useState(undefined);
- 
+  //  Fix suggested in Slack set state to undefined 
+  // and wrap return below in if statement. 
+
   useEffect(() => {
 
     const id = props.match.params.id;
 
-       axios
-        .get(`http://localhost:5000/api/movies/${id}`)
-        .then(response => {
-          setMovie(response.data);
-        })
-        .catch(error => {
-          console.error(error);
-        });
+    axios
+      .get(`http://localhost:5000/api/movies/${id}`)
+      .then(response => {
+        setMovie(response.data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
 
-  },[props.match.params.id]);
-  
+  }, [props.match.params.id]);
+
   // Uncomment this only when you have moved on to the stretch goals
   // const saveMovie = () => {
   //   const addToSavedList = props.addToSavedList;
@@ -33,29 +36,30 @@ const Movie = (props) => {
   }
 
   const { title, director, metascore, stars } = movie;
-  if(movie !== undefined){
-  return (
-    <div className="save-wrapper">
-      <div className="movie-card">
-        <h2>{title}</h2>
-        <div className="movie-director">
-          Director: <em>{director}</em>
-        </div>
-        <div className="movie-metascore">
-          Metascore: <strong>{metascore}</strong>
-        </div>
-        <h3>Actors</h3>
-
-        {stars.map(star => (
-          <div key={star} className="movie-star">
-            {star}
+  // Suggested fix to workaround map issue (If statement)
+  if (movie !== undefined) {
+    return (
+      <div className="save-wrapper">
+        <div className="movie-card">
+          <h2>{title}</h2>
+          <div className="movie-director">
+            Director: <em>{director}</em>
           </div>
-        ))}
+          <div className="movie-metascore">
+            Metascore: <strong>{metascore}</strong>
+          </div>
+          <h3>Actors</h3>
+
+          {stars.map(star => (
+            <div key={star} className="movie-star">
+              {star}
+            </div>
+          ))}
+        </div>
+        <div className="save-button">Save</div>
       </div>
-      <div className="save-button">Save</div>
-    </div>
-  );
-        }
+    );
+  }
 }
 
 export default Movie;
